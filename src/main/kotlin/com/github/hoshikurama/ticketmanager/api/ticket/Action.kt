b5a812f2ccfae03933e433a98d5faa6ae698f749
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2023-06-04 16:51:10 -0700

    Fixed issue where it is impossible to store assignments polymorphically

--- src/main/kotlin/com/github/hoshikurama/ticketmanager/api/ticket/Action.kt
+++ src/main/kotlin/com/github/hoshikurama/ticketmanager/api/ticket/Action.kt
@@ -2,22 +2,30 @@ package com.github.hoshikurama.ticketmanager.api.ticket
 
 import java.time.Instant
 
+
 /**
- * Represents any action done upon a ticket, including creation. Actions are immutable and thread-safe.
+ * Represents any action done upon a ticket, including creation. Actions are immutable and thread-safe. Please note that
+ * this interface does not actually contain any data. It is used to hold a reference to a particular action, where you must
+ * then discover the type to get access to its data via the ActionInfo outer class.
+ */
+sealed interface Action
+
+/**
+ * Contains the non-action-specific data for an action. The inner classes represent particular action types.
  * @property user user who performed the action on the ticket
  * @property location location where user performed the action on the ticket
  * @property timestamp Epoch time for when the action was performed
  */
-class Action(
-    val user: Assignment,
-    val location: ActionLocation,
-    val timestamp: Long = Instant.now().epochSecond,
+class ActionInfo(
+     val user: Assignment,
+     val location: ActionLocation,
+     val timestamp: Long = Instant.now().epochSecond,
 ) {
 
     /**
      * Ticket assignment action
      */
-    inner class Assign(val assignment: Assignment)
+    inner class Assign(val assignment: Assignment): Action
     /**
      * Ticket comment action
      */
