35d977f3529837ade823b151064ac771d7365b69
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2023-06-04 18:00:44 -0700

    Split contents into a new interface. Actions now store have reference to outer class variables without ActionInfo being a valid Action type!

--- src/main/kotlin/com/github/hoshikurama/ticketmanager/api/ticket/Action.kt
+++ src/main/kotlin/com/github/hoshikurama/ticketmanager/api/ticket/Action.kt
@@ -4,11 +4,19 @@ import java.time.Instant
 
 
 /**
- * Represents any action done upon a ticket, including creation. Actions are immutable and thread-safe. Please note that
- * this interface does not actually contain any data. It is used to hold a reference to a particular action, where you must
- * then discover the type to get access to its data via the ActionInfo outer class.
+ * Contains the contents which every action has. These are stored in the ActionInfo outer class.
  */
-sealed interface Action
+sealed interface ActionContents {
+    val user: Creator
+    val location: ActionLocation
+    val timestamp: Long
+}
+
+/**
+ * Represents any action done upon a ticket, including creation. Actions are immutable and thread-safe. Please note that
+ * this interface does not actually contain any extra. It is used for polymorphism purposes.
+ */
+sealed interface Action : ActionContents
 
 /**
  * Contains the non-action-specific data for an action. The inner classes represent particular action types.
@@ -17,43 +25,42 @@ sealed interface Action
  * @property timestamp Epoch time for when the action was performed
  */
 class ActionInfo(
-     val user: Creator,
-     val location: ActionLocation,
-     val timestamp: Long = Instant.now().epochSecond,
-) {
+     override val user: Creator,
+     override val location: ActionLocation,
+     override val timestamp: Long = Instant.now().epochSecond,
+) : ActionContents {
 
     /**
      * Ticket assignment action
      */
-    inner class Assign(val assignment: Assignment): Action
+    inner class Assign(val assignment: Assignment): Action, ActionContents by this@ActionInfo
     /**
      * Ticket comment action
      */
-    inner class Comment(val comment: String): Action
+    inner class Comment(val comment: String): Action, ActionContents by this@ActionInfo
     /**
      * Closes a ticket while also leaving a comment.
      */
-    inner class CloseWithComment(val comment: String): Action
+    inner class CloseWithComment(val comment: String): Action, ActionContents by this@ActionInfo
 
     /**
      * Opening ticket. Note: the initial message is contained here.
      */
-    inner class Open(val message: String): Action
+    inner class Open(val message: String): Action, ActionContents by this@ActionInfo
     /**
      * Priority-Change ticket action.
      */
-    inner class SetPriority(val priority: Ticket.Priority): Action
+    inner class SetPriority(val priority: Ticket.Priority): Action, ActionContents by this@ActionInfo
     /**
      * Closing ticket action. Note: This is a pure close.
      */
-    inner class CloseWithoutComment: Action
+    inner class CloseWithoutComment: Action, ActionContents by this@ActionInfo
     /**
      * Re-open ticket action.
      */
-    inner class Reopen: Action
+    inner class Reopen: Action, ActionContents by this@ActionInfo
     /**
      * Mass-Close ticket action.
      */
-    inner class MassClose: Action
+    inner class MassClose: Action, ActionContents by this@ActionInfo
 }
-
