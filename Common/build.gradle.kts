556c8ee3d085a4ff17306f99a9d901cbe7d5a448
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2023-08-06 13:34:47 -0700

    Update Dependencies

--- Common/build.gradle.kts
+++ Common/build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
-    kotlin("jvm") version "1.8.22"
+    kotlin("jvm") version "1.9.0"
+    id("com.github.ben-manes.versions") version "0.47.0"
 }
 
 dependencies {
-    compileOnly("net.kyori:adventure-api:4.13.1")
+    compileOnly("net.kyori:adventure-api:4.14.0")
     compileOnly("net.luckperms:api:5.4")
     compileOnly("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.7.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
-    implementation("net.kyori:adventure-api:4.13.1")
-    implementation("net.kyori:adventure-text-minimessage:4.13.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("net.kyori:adventure-text-minimessage:4.14.0")
     implementation(kotlin("stdlib-jdk8"))
 }
 
