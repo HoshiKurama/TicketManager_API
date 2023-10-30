077f1491ff8e5b25b78df21612c62dc8a2445c5b
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2023-10-30 10:42:27 -0700

    Abstract away TMSE-only functions

--- Common/src/main/kotlin/com.github.hoshikurama.ticketmanager.api/impl/TicketManager.kt
+++ Common/src/main/kotlin/com.github.hoshikurama.ticketmanager.api/impl/TicketManager.kt
@@ -1,14 +1,21 @@
 package com.github.hoshikurama.ticketmanager.api.impl
 
 import com.github.hoshikurama.ticketmanager.api.impl.registry.*
+import com.github.hoshikurama.ticketmanager.api.registry.config.ConfigRegistry
+import com.github.hoshikurama.ticketmanager.api.registry.database.DatabaseRegistry
+import com.github.hoshikurama.ticketmanager.api.registry.locale.LocaleRegistry
+import com.github.hoshikurama.ticketmanager.api.registry.permission.PermissionRegistry
+import com.github.hoshikurama.ticketmanager.api.registry.playerjoin.PlayerJoinRegistry
+import com.github.hoshikurama.ticketmanager.api.registry.precommand.PreCommandRegistry
+import com.github.hoshikurama.ticketmanager.api.registry.repeatingtasks.RepeatingTaskRegistry
 
 object TicketManager {
-    val ConfigRegistry = TMConfigRegistry()
-    val DatabaseRegistry = TMDatabaseRegistry()
-    val LocaleRegistry = TMLocaleRegistry()
-    val PermissionRegistry = TMPermissionRegistry()
-    val PlayerJoinRegistry = TMPlayerJoinRegistry()
-    val PreCommandRegistry = TMPreCommandRegistry()
-    val RepeatingTaskRegistry = TMRepeatingTaskRegistry()
+    val ConfigRegistry: ConfigRegistry = TMConfigRegistry()
+    val DatabaseRegistry: DatabaseRegistry = TMDatabaseRegistry()
+    val LocaleRegistry: LocaleRegistry = TMLocaleRegistry()
+    val PermissionRegistry: PermissionRegistry = TMPermissionRegistry()
+    val PlayerJoinRegistry: PlayerJoinRegistry = TMPlayerJoinRegistry()
+    val PreCommandRegistry: PreCommandRegistry = TMPreCommandRegistry()
+    val RepeatingTaskRegistry: RepeatingTaskRegistry = TMRepeatingTaskRegistry()
     val EventBus = TMEventBus()
 }
